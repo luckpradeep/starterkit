4dbc5ebcb3c78ce07c22f675ce4d7868ee6c29fa
Pradeep <luckpradeep@gmail.com>
2016-12-11 08:21:11 +0000

    json mock server for c9.io

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,14 @@
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:8081/' : '/';
+  const hostname = window.location.hostname;
+  
+  if (hostname === 'localhost') {
+    return 'http://localhost:8081/';
+  } else if (hostname === 'starterkit-luckpradeep.c9users.io') {
+    return 'https://starterkit-luckpradeep.c9users.io:8081/';
+  } else {
+    return '/'
+  }
+  //const inDevelopment = window.location.hostname === 'localhost' || ;
+  //return inDevelopment ? 'http://localhost:8081/' : '/';
+  //return 'https://starterkit-luckpradeep.c9users.io:8081/';
 }
