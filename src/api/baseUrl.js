a56dfcc2c3c9397eaad172f5cc05f6c7def4ee0d
Pradeep <luckpradeep@gmail.com>
2016-12-13 05:27:33 +0000

    production environment set working for local

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -4,8 +4,20 @@ export default function getBaseUrl() {
   if (hostname === 'localhost') {
     return 'http://localhost:8081/';
   } else if (hostname === 'starterkit-luckpradeep.c9users.io') {
-    return 'https://starterkit-luckpradeep.c9users.io:8081/';
+    return 'https://starterkit-luckpradeep.c9users.io/';
+    // todo luckpradeep for webpack.config.prod need to set it to /
+    //return 'https://starterkit-luckpradeep.c9users.io:8081/';
   } else {
     return '/'
   }
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g), ' ');
 }
